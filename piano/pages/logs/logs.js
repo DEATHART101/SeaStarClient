15f043da98d875ac36f5dd8010a87afe3469332a
utako <15110285591@163.com>
2018-12-15 14:46:13 +0800

    add

--- piano/pages/logs/logs.js
+++ piano/pages/logs/logs.js
@@ -13,7 +13,39 @@ Page({
   //跳转到选择登录方式界面
   loginBtn: function(options){
     wx.navigateTo({
-      url:'../login/login'
+      url:'../outlogin/outlogin'
     })
-  }
+  },
+
+  //其余六个按钮跳转函数
+  my_info_btn: function () {
+    wx.navigateTo({
+      
+    })
+  },
+  modify_info_btn: function () {
+    wx.navigateTo({
+
+    })
+  },
+  now_order_btn: function () {
+    wx.navigateTo({
+
+    })
+  },
+  previous_order_btn: function () {
+    wx.navigateTo({
+
+    })
+  },
+  notify_btn: function () {
+    wx.navigateTo({
+
+    })
+  },
+  longtime_order_btn: function () {
+    wx.navigateTo({
+
+    })
+  },
 })
